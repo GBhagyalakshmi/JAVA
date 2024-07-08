25f5b2ebcb1852fcb0c145a785cbef899bbbabcf
gunda <gunda@bhagyalakshmi>
2024-07-08 21:58:18 +0530

    fourth commit program

---
+++ JAVA/src/com/tnsif/scannere/Scannerexample.java
@@ -0,0 +1,13 @@
+package com.tnsif.scannere;
+
+import java.util.Scanner;
+
+public class Scannerexample {
+	public static void main (String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter your are name :");
+		String name =sc.nextLine();
+		System.out.println("Enter your rollnumber:");
+		int roll =sc.nextInt();
+		}
+}
