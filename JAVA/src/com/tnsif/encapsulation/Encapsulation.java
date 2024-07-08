31ae8c63eac33c02b40115cdae5b01eaf257b571
gunda <gunda@bhagyalakshmi>
2024-07-08 21:51:14 +0530

    third commit

---
+++ JAVA/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,13 @@
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+		int Serialnumber;
+		String name;
+		int age;
+		void show() {
+			System.out.println("Serialnumber:"+Serialnumber+"name:"+name);
+		}
+
+	}
+
+
