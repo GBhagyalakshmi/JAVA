2f8447d5061c14f2d8d2f160c78285d293aa3837
gunda <gunda@bhagyalakshmi>
2024-07-08 22:02:15 +0530

    first commit-ii

---
+++ JAVA/src/com/tnsif/java/Publicdemo.java
@@ -0,0 +1,11 @@
+package com.tnsif.java;
+
+public class Publicdemo {
+		 private int a=2;
+		public static void main (String args[]) {
+			Publicdemo p=new Publicdemo();
+			System.out.println(p.a);
+		}
+
+
+}
