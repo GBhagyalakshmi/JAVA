3ed8faf65e3231321bf0bc6055b0a59a9a97b99d
gunda <gunda@bhagyalakshmi>
2024-07-19 00:26:56 +0530

    String Sequencemethod

---
+++ JAVA/src/com/tnsif/programs/Reversing.java
@@ -0,0 +1,12 @@
+package com.tnsif.programs;
+
+
+public class Reversing {
+	public static void main(String[] args) {
+	String[] numlist= {"B","h","a","g","y","a"};
+	for (String eachword:numlist) {
+		System.out.println(eachword);
+	}
+
+}
+}
