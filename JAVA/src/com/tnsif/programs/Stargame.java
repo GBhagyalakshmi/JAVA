217727abc2b580913532843857b2b1815deb509b
gunda <gunda@bhagyalakshmi>
2024-07-19 01:16:50 +0530

    table multiplication

---
+++ JAVA/src/com/tnsif/programs/Stargame.java
@@ -0,0 +1,30 @@
+package com.tnsif.programs;
+import java.util.Scanner; 
+
+public class Stargame {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int num=sc.nextInt();
+		System.out.println("The Table of "+num);
+		System.out.println(num*1);
+		System.out.println(num*2);
+		System.out.println(num*3);
+		System.out.println(num*4);
+		System.out.println(num*5);
+		System.out.println(num*6);
+		System.out.println(num*7);
+		System.out.println(num*8);
+		System.out.println(num*9);
+		System.out.println(num*10);
+		System.out.println("It's done");
+
+		
+
+   }
+
+	private static int String(int num) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+}
+
